4e6d839445d4a569bcb15053227a9cc865effe7f
Daniel <danieltroyaromin1337@gmail.com>
2024-08-18 18:09:18 +0800

    Feature - Door Open/Close * Open and close by dragging * Still need fixes

--- Assets/_scripts/PlayerController.cs
+++ Assets/_scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -34,6 +35,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     #endregion
 
+    #region Hold Object
     [Header("Hold Object")]
     private RaycastHit hit;
     [SerializeField] Transform holdArea;
@@ -47,6 +49,15 @@ public class PlayerController : MonoBehaviour
     [Header("Physics Parameters")]
     [SerializeField] private float objectHoldRange = 5.0f;
     [SerializeField] private float holdupForce = 150.0f;
+    #endregion
+
+    #region Open/Close Door
+    [SerializeField] private LayerMask doorLayer;
+    private Transform selectedDoor;
+    private GameObject dragPointGameobject;
+    private int leftDoor = 0;
+    [SerializeField] private Camera cam;
+    #endregion
 
     private void Start()
     {
@@ -69,6 +80,8 @@ public class PlayerController : MonoBehaviour
 
         HoldObject();
         SetCursor();
+
+        OpenCloseDoor();
     }
 
     private void FixedUpdate()
@@ -104,7 +117,10 @@ public class PlayerController : MonoBehaviour
             {
                 if (InteractableDetected())
                 {
-                    PickupObject(hit.transform.gameObject);
+                    if (hit.collider.CompareTag("HoldableObject"))
+                    {
+                        PickupObject(hit.transform.gameObject);
+                    }
                 }
             }
             else
@@ -204,6 +220,90 @@ public class PlayerController : MonoBehaviour
 
     #region Hold Up Object
 
+    #endregion
+
+    #region Door
+    private void OpenCloseDoorTest()
+    {
+     
+    }
+
+    private void OpenCloseDoor()
+    {
+        Debug.Log("Her");
+
+        RaycastHit doorHit;
+
+        if (Physics.Raycast(cameraDirection.position, cameraDirection.forward, out doorHit, 20, doorLayer))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                selectedDoor = doorHit.collider.gameObject.transform;
+            }
+        }
+
+        if (selectedDoor != null)
+        {
+            HingeJoint joint = selectedDoor.GetComponent<HingeJoint>();
+            JointMotor motor = joint.motor;
+
+            if (dragPointGameobject == null)
+            {
+                dragPointGameobject = new GameObject("Ray door");
+                dragPointGameobject.transform.parent = selectedDoor;
+            }
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            dragPointGameobject.transform.position = ray.GetPoint(Vector3.Distance(selectedDoor.position, transform.position));
+            dragPointGameobject.transform.rotation = selectedDoor.rotation;
+
+            float delta = Mathf.Pow(Vector3.Distance(dragPointGameobject.transform.position, selectedDoor.position), 3);
+
+            if (selectedDoor.GetComponent<MeshRenderer>().localBounds.center.x > selectedDoor.localPosition.x)
+            {
+                leftDoor = 1;
+            }
+            else
+            {
+                leftDoor = -1;
+            }
+
+            //Applying velocity to door motor
+            float speedMultiplier = 60000;
+            if (Mathf.Abs(selectedDoor.parent.forward.z) > 0.5f)
+            {
+                if (dragPointGameobject.transform.position.x > selectedDoor.position.x)
+                {
+                    motor.targetVelocity = delta * -speedMultiplier * Time.deltaTime * leftDoor;
+                }
+                else
+                {
+                    motor.targetVelocity = delta * speedMultiplier * Time.deltaTime * leftDoor;
+                }
+            }
+            else
+            {
+                if (dragPointGameobject.transform.position.z > selectedDoor.position.z)
+                {
+                    motor.targetVelocity = delta * -speedMultiplier * Time.deltaTime * leftDoor;
+                }
+                else
+                {
+                    motor.targetVelocity = delta * speedMultiplier * Time.deltaTime * leftDoor;
+                }
+            }
+
+            joint.motor = motor;
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                selectedDoor = null;
+                motor.targetVelocity = 0;
+                joint.motor = motor;
+                Destroy(dragPointGameobject);
+            }
+        }
+    }
     #endregion
 
     private void OnDrawGizmos()
